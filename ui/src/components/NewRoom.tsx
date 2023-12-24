b96c244627412e1875f0a490bac27c1d4815e7c2
mohamedeliwa <mohamed.av11@gmail.com>
2023-12-24 18:40:38 +0200

    making new room compnent use css module instead of tailwind

---
+++ ui/src/components/NewRoom.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import styles from "./NewRoom.module.css";
+
+const NewRoom: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [phone, setPhone] = useState<string>("");
+
+  const handleRoomCreation = async () => {
+    try {
+      const item = window.localStorage.getItem("user");
+      let user = item ? JSON.parse(item) : "";
+      if (!user) return;
+
+      const url = `http://localhost:8080/rooms/${user?.id}`;
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ phone }),
+      });
+
+      // in case of request error
+      if (response.status < 200 || response.status > 200) {
+        const msg = await response.text();
+        alert(`${msg}. \nplease try again!`);
+        return;
+      }
+
+      // reload to fetch the newly updated room list
+      window.location.reload();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {!open ? (
+        <button
+          onClick={() => setOpen(true)}
+          className={`${styles.base_input} ${styles.btn} ${styles.new_room_btn}`}
+        >
+          New Room
+        </button>
+      ) : (
+        <>
+          <button
+            onClick={handleRoomCreation}
+            className={`${styles.base_input} ${styles.btn} ${styles.create_room_btn}`}
+          >
+            Create
+          </button>
+          <button
+            onClick={() => {
+              setPhone("");
+              setOpen(false);
+            }}
+            className={`${styles.base_input} ${styles.btn} ${styles.cancel_btn}`}
+          >
+            Cancel
+          </button>
+        </>
+      )}
+      {open && (
+        <form>
+          <input
+            value={phone}
+            onChange={(e) => {
+              const value = e.target.value;
+              setPhone(value);
+            }}
+            className={`${styles.base_input} ${styles.input}`}
+          />
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default NewRoom;

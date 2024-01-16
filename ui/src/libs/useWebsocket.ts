a0d7c42cd887d2b0cface3eba922efb953196174
mohamedeliwa <mohamed.av11@gmail.com>
2024-01-16 17:24:10 +0200

    building basic use websocket hook

---
+++ ui/src/libs/useWebsocket.ts
@@ -0,0 +1,39 @@
+"use client";
+
+//  This Hook is for connecting to the WebSocket server, enabling us to send and receive messages:
+import { useEffect, useRef } from "react";
+import useUser from "./useUser";
+
+export default function useWebsocket(onMessage: Function) {
+  const ws = useRef<WebSocket | null>(null);
+  let user = useUser();
+
+  useEffect(() => {
+    if (ws.current !== null) return;
+    const wsUri = `ws://localhost:8080/ws/${user?.id || ""}`;
+    ws.current = new WebSocket(wsUri);
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+    return () => {
+      // cleaning on unmounting
+      if (ws.current) {
+        ws.current.close();
+        ws.current = null;
+      }
+    };
+  }, [user?.id]);
+
+  useEffect(() => {
+    if (!ws.current) return;
+    ws.current.onmessage = (e) => {
+      onMessage(e.data);
+    };
+  }, [onMessage]);
+
+  const sendMessage = (msg: string) => {
+    if (!ws.current) return;
+    ws.current.send(msg);
+  };
+
+  return sendMessage;
+}
